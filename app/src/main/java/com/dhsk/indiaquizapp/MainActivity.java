d9b1c01bf6e2635fa06007ee7a9055792faef3ba
dtejwani <55606729+dtejwani@users.noreply.github.com>
2020-06-11 17:09:00 +0530

    Third

--- app/src/main/java/com/dhsk/indiaquizapp/MainActivity.java
+++ app/src/main/java/com/dhsk/indiaquizapp/MainActivity.java
@@ -16,50 +16,64 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    int flag=0;
-    int score=0;
-    int ques=1;
 
-    int num=0;
+    int flag = 0;
+    int score = 0;
+    int ques = 1;
+
+    int num = 0;
 
     public void onRadioButtonClicked(View view) {
         // Is the button now checked?
         boolean checked = ((RadioButton) view).isChecked();
 
         // Check which radio button was clicked
-        switch(view.getId()) {
+        switch (view.getId()) {
             case R.id.threetrue:
                 if (checked)
-                    flag=1;
-                    break;
+                    flag = 1;
+                break;
             case R.id.threefalse:
                 if (checked)
-                    flag=0;
-                    break;
+                    flag = 0;
+                break;
         }
     }
-    public void Submit_Answers(View v){
-        if(flag==1){score++;}
-        CheckBox checkone=(CheckBox) findViewById(R.id.checkone);
-        CheckBox checktwo=(CheckBox) findViewById(R.id.checktwo);
-        CheckBox checkthree = (CheckBox) findViewById(R.id.checkthree);
-        if(checkthree.isChecked()){ num=1;}
-        if ((checkone.isChecked())&&(checktwo.isChecked())&&(num==0)){ score++;}
 
-        EditText nameText=(EditText) findViewById(R.id.ansone);
+    public void Submit_Answers(View v) {
+        if (flag == 1) {
+            score++;
+        }
+        CheckBox checkone = (CheckBox) findViewById(R.id.checkone);
+        CheckBox checktwo = (CheckBox) findViewById(R.id.checktwo);
+        CheckBox checkthree = (CheckBox) findViewById(R.id.checkthree);
+        if (checkthree.isChecked()) {
+            num = 1;
+        }
+        if ((checkone.isChecked()) && (checktwo.isChecked()) && (num == 0)) {
+            score++;
+        }
+
+        EditText nameText = (EditText) findViewById(R.id.ansone);
         String ansone = nameText.getText().toString();
-        String one="1947";
-        if(ansone.equals(one)){score++;}
-        EditText anstwoo=(EditText) findViewById(R.id.anstwo);
+        String one = "1947";
+        if (ansone.equalsIgnoreCase(one)) {
+            score++;
+        }
+        EditText anstwoo = (EditText) findViewById(R.id.anstwo);
         String anstwo = anstwoo.getText().toString();
-        String two="BABUR";
-        if(anstwo.equalsIgnoreCase(two)){score++;}
-        EditText ansfivee=(EditText) findViewById(R.id.ansfive);
+        String two = "BABUR";
+        if (anstwo.equalsIgnoreCase(two)) {
+            score++;
+        }
+        EditText ansfivee = (EditText) findViewById(R.id.ansfive);
         String ansfive = ansfivee.getText().toString();
-        String five="GANDHI";
-        if(ansfive.equalsIgnoreCase(five)){score++;}
-        Toast.makeText(this,"You scored "+ score+" in the quiz", Toast.LENGTH_SHORT).show();
-        score=0;
+        String five = "GANDHI";
+        if (ansfive.equalsIgnoreCase(five)) {
+            score++;
+        }
+        Toast.makeText(this, "You scored " + score + " in the quiz", Toast.LENGTH_SHORT).show();
+        score = 0;
 
 
     }
